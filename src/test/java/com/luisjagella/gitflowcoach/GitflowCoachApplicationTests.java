79a50c7a9096d59896f87539a1bdd6ec9bee9692
Luís <luisprinces950@gmail.com>
2026-07-15 02:14:48 -0300

    test: configura banco em memória para testes

--- src/test/java/com/luisjagella/gitflowcoach/GitflowCoachApplicationTests.java
+++ src/test/java/com/luisjagella/gitflowcoach/GitflowCoachApplicationTests.java
@@ -2,12 +2,14 @@ package com.luisjagella.gitflowcoach;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GitflowCoachApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
