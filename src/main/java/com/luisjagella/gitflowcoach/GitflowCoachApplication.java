384a272aecade00a3bbf2adf2a16658e118fd196
Luís <luisprinces950@gmail.com>
2026-07-15 01:04:10 -0300

    chore(spring-boot): cria estrutura inicial do projeto

---
+++ src/main/java/com/luisjagella/gitflowcoach/GitflowCoachApplication.java
@@ -0,0 +1,13 @@
+package com.luisjagella.gitflowcoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitflowCoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitflowCoachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisjagella/gitflowcoach/GitflowCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisjagella.gitflowcoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitflowCoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
